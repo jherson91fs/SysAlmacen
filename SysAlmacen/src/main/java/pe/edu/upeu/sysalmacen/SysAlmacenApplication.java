182459546e09fe490275af6714813f2b92ab55fb
Renzo-Juli-M <renzojuli717@gmail.com>
2025-05-13 16:13:43 -0500

    fesfs

--- SysAlmacen/src/main/java/pe/edu/upeu/sysalmacen/SysAlmacenApplication.java
+++ SysAlmacen/src/main/java/pe/edu/upeu/sysalmacen/SysAlmacenApplication.java
@@ -2,7 +2,6 @@ package pe.edu.upeu.sysalmacen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class SysAlmacenApplication {
 
